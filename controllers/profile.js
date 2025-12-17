5cd5f1e451fe86999f505874573470a70fb5d7d8
fadhelhaji <fadhelmh@hotmail.com>
2025-12-17 22:26:57 +0300

    added more css to the profile dashboard

--- controllers/profile.js
+++ controllers/profile.js
@@ -48,4 +48,37 @@ router.delete('/delete-account', async (req, res) => {
   }
 });
 
+router.get('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user.isPublic) {
+      return res.send('This user’s logs are not public');
+    }
+
+    const meals = await Meals.find({ userId: user._id }).populate('userId').sort({ date: 1 });
+
+    let mealsByDay = {};
+    meals.forEach(meal => {
+      const day = meal.date.toISOString().split('T')[0];
+      if (!mealsByDay[day]) mealsByDay[day] = [];
+      mealsByDay[day].push(meal);
+    });
+
+
+    const exercises = await Exercises.find({ userId: user._id }).populate('userId');
+    const plan = await Plan.find({ userId: user._id });
+
+    let exercisesByDay = {};
+    exercises.forEach(ex => {
+      const day = ex.date ? ex.date.toISOString().split('T')[0] : 'Unknown';
+      if (!exercisesByDay[day]) exercisesByDay[day] = [];
+      exercisesByDay[day].push(ex);
+    });
+    res.render('appDashboard/dashboard.ejs', { user, exercisesByDay, plan, mealsByDay });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 module.exports = router;
